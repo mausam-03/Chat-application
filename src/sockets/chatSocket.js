49cab50a3326bde2838a9757fadbff90ca926897
Mausam Jha <mausamjha2503@gmail.com>
2026-03-21 18:15:42 +0530

    added authentication middleware in chatsocket.js file to provide user id

--- src/sockets/chatSocket.js
+++ src/sockets/chatSocket.js
@@ -1,9 +1,38 @@
 import prisma from "../config/client.js";
 
-export const chatSocket = (io) => {
-  io.on("connection", (socket) => {
+import { verifyAccessToken } from "../utils/token.js";
 
-    console.log("User connected:", socket.id);
+export const chatSocket = (io) => {
+  // Authentication Middleware
+  io.use((socket, next) => {
+    try {
+      const token = socket.handshake.auth?.token || socket.handshake.headers?.authorization?.split(" ")[1];
+      if (!token) {
+        return next(new Error("Authentication error: Token missing"));
+      }
+      const decoded = verifyAccessToken(token);
+      socket.user = decoded;
+      next();
+    } catch (err) {
+      return next(new Error("Authentication error: Invalid or expired token"));
+    }
+  });
+
+  io.on("connection", async (socket) => {
+    socket.userId = socket.user.id;
+    console.log(`User connected: ${socket.id} (UserId: ${socket.userId})`);
+
+    // Update online status
+    try {
+      await prisma.user.update({
+        where: { id: socket.userId },
+        data: { isOnline: true },
+      });
+      // Broadcast presence to all other connected clients
+      socket.broadcast.emit("presence_update", { userId: socket.userId, isOnline: true });
+    } catch (err) {
+      console.error("Failed to update user presence on connection:", err);
+    }
 
     // Join conversation room
     socket.on("join_conversation", (conversationId) => {
